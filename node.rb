054a1a1cea1203df3bc2b0cf1437299298b094d7
Vincent Klimjack <vklimjack@gmail.com>
2020-12-04 16:00:58 +0000

    added node class

--- node.rb
+++ node.rb
@@ -1,3 +1,12 @@
 class Node
-  
+  attr_reader :coordinates
+
+  def initialize(coordinates)
+    @coordinates = coordinates
+    @adjacent_nodes = []
+  end
+
+  def add_edge(adjacent_node)
+    @adjacent_nodes << adjacent_node
+  end
 end
