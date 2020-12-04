61cfa1f725a8b463a3926b6e54921754ed0d3bb2
Vincent Klimjack <vklimjack@gmail.com>
2020-12-04 07:12:38 +0000

    added basic knight moves

--- knight.rb
+++ knight.rb
@@ -1,3 +1,7 @@
 class Knight
-
+  BASIC_MOVES =
+    [
+      [1, 2], [2, 1], [-1, 2], [-2, 1], # upward moves
+      [1, -2], [2, -1], [-1, -2], [-2, -1] # downward moves
+    ].freeze
 end
--- node.rb
+++ node.rb
@@ -1,3 +1,3 @@
 class Node
-
+  
 end
