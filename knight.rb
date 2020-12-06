4c3b59e0e5a54c90c5e488086d0f3121c11b0cce
Vincent Klimjack <vklimjack@gmail.com>
2020-12-06 11:38:18 +0000

    added method to find possible moves from a starting position

--- knight.rb
+++ knight.rb
@@ -1,7 +1,18 @@
+require 'pry'
+
 class Knight
   BASIC_MOVES =
     [
       [1, 2], [2, 1], [-1, 2], [-2, 1], # upward moves
       [1, -2], [2, -1], [-1, -2], [-2, -1] # downward moves
     ].freeze
+
+  def possible_moves(current_position)
+    possible_coordinates = []
+    BASIC_MOVES.each do |move|
+      new_coordinates = move.zip(current_position).map { |x, y| x + y if (x + y).between?(-7, 7) }
+      possible_coordinates << new_coordinates unless new_coordinates.include?(nil)
+    end
+    possible_coordinates
+  end
 end
