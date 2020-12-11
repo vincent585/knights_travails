f8ff3a4a2865b754df3c6208c2daaf5aadfe5ae9
Vincent Klimjack <vklimjack@gmail.com>
2020-12-11 07:34:55 +0000

    added knight_moves method, 99% complete!

--- board.rb
+++ board.rb
@@ -19,15 +19,28 @@ class Board
       unless discovered.include?(move)
         discovered << move
         queue << move
+        move.parent = current_node
       end
     end
   end
 
   def knight_moves(start, target)
-    # TODO
+    last_node = bfs(start, target)
+    return if last_node.nil?
+
+    path = retrieve_parent_nodes(last_node)
+    path.each { |node| p node.coordinates }
+  end
+
+  def retrieve_parent_nodes(last_node)
+    path = [last_node]
+    until last_node.parent.nil?
+      path.unshift(last_node.parent)
+      last_node = last_node.parent
+    end
+    path
   end
 end
 
 x = Board.new
-p x.bfs([0, 0], [1, 2])
-p x.bfs([0, 0], [7, 7])
+x.knight_moves([0, 0], [7, 7])
--- knight.rb
+++ knight.rb
@@ -16,7 +16,7 @@ class Knight
   def possible_moves(current_position = @coordinates, possible_coordinates = @adjacent_nodes)
     BASIC_MOVES.each do |move|
       new_coordinates = move.zip(current_position).map { |x, y| x + y if (x + y).between?(0, 7) }
-      possible_coordinates << Knight.new(new_coordinates, current_position) unless new_coordinates.include?(nil)
+      possible_coordinates << Knight.new(new_coordinates) unless new_coordinates.include?(nil)
     end
     possible_coordinates
   end
